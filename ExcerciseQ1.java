53f84f3e94c93dbe8f301afb61f78d3464e50966
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-06 15:09:39 +0530

    Add files via upload

---
+++ ExcerciseQ1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ExcerciseQ1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the marks of subject 1");
+		byte Subject1 = sc.nextByte();
+		System.out.println("Enter the marks of subject 2");
+		byte Subject2 = sc.nextByte();
+		System.out.println("Enter the marks of subject 3");
+		byte Subject3 = sc.nextByte();
+		
+		
+		double avg = (Subject1 + Subject2 + Subject3)/3;
+		System.out.println("Average Marks"+ avg);
+		
+		if(avg >= 40 && Subject1 >=33 && Subject2 >=33 && Subject3 >=33) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		
+	}
+
+}
